e446fcb479391ea015985f393b2fb61858f91fd3
Samuel FORESTIER <dev@samuel.domains>
2020-02-29 20:07:02 +0100

    Updates the Semver REGEXP (sindresorhus/semver-regex@v3.1.1)

--- CDNConstants.py
+++ CDNConstants.py
@@ -2,10 +2,10 @@
 
 # pylint: disable=line-too-long
 # This regex has been written by @sindresorhus for Semver.
-# <https://github.com/sindresorhus/semver-regex>
-SEMVER_REGEX = r"v?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?"
+# <https://github.com/sindresorhus/semver-regex> (v3.1.1)
+SEMVER_REGEX = r"(?<=^v?|\sv?)(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:0|[1-9]\d*|[\da-z-]*[a-z-][\da-z-]*)(?:\.(?:0|[1-9]\d*|[\da-z-]*[a-z-][\da-z-]*))*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?(?=$|\s)"
 
-# This is a regex written by @diegoperini, Python ported by @adamrofer.
+# This is a regex written by @diegoperini, ported for Python by @adamrofer.
 # <https://gist.github.com/dperini/729294>
 # It has been tweaked to work with network path references and HTML tags.
 LINK_REGEX = r"(?:(https?:)?//)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S[^\"\s]*)?"
